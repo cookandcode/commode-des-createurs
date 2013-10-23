c40b0729f06dedb84f67cb95afb1177555bfd10a
Georges <georges@PC-de-Georges.local>
2013-10-23 11:17:01 +0200

    init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CommodeDesCreateurs::Application.config.secret_key_base = 'ec6e8270a1d074314dac13525aaee2912e54372069a90fcce2eab6929eb16bf2354e3284230936e922b3ad8be0da524bc46ea34e48738ffdf702a2c5aa206aa9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CommodeDesCreateurs::Application.config.session_store :cookie_store, key: '_commode-des-createurs_session'
